02f0f201e1f6f64faf08c782db2ac115074add4f
Cyrille MBIA <cyrille@itdreamreal.com>
2019-08-22 15:17:05 +0200

    Initial commit

---
+++ app/src/main/java/com/mbia/cyrille/localnotif/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mbia.cyrille.localnotif;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
